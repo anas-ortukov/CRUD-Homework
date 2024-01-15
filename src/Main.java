f61305a27ee712a22bc067be571def1e4674638f
John <azizortukov818@gmail.com>
2024-01-15 17:12:55 +0500

    Project Testing (By John)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import entity.Student;
+import repository.StudentRepo;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        StudentRepo studentRepo = StudentRepo.getInstance();
+        for (Student student : studentRepo.findAll()) {
+            System.out.println(student.getFirstName() + " " + student.getLastName());
+        }
+
 
     }
 }
